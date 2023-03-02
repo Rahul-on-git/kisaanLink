c2b606b63d7481631bd45f2abad6a697a2e93a96
rahul-on-git <2020.rahul.motwani@ves.ac.in>
2023-03-02 15:21:49 +0530

    made framerModelSchema for DB. Fields are Str type

--- modules/farmer.js
+++ modules/farmer.js
@@ -2,5 +2,24 @@ const mongoose = require("mongoose");
 
 const schema = mongoose.schema;
 
-const farmerSchema = new schema();
+const farmerSchema = new schema(
+    {
+        farmerName: {
+            type: String,
+            required: true
+        },
+        farmerContact:{
+            type: String,
+            required: true
+        },
+        farmerLocation:{
+            type: String,
+            required: true
+        },
+        farmerPass:{
+            type: String,
+            required: true
+        }
+    }
+);
 
