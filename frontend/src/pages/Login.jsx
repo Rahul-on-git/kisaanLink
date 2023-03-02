96b4bd8b9d770a873147c49743996038965c4ba8
malharkajale <malharkajale@gmail.com>
2023-03-02 20:38:22 +0530

    add login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react"
+
+function Login() {
+    const  [contact, setContact] = useState('');
+    const  [password, setPassword] = useState('');
+
+
+    return (
+        <div className="register-login">
+            <fieldset className="radio-buttons">
+                {/* <button>Customer</button>
+                <button>Farmer</button> */}
+                <legend>Select user type: </legend>
+                <div>
+                    <input type="radio" id="customer" name="userGroup" value="Customer" checked/>
+                    <label for="customer">Customer</label>
+                </div>
+                <div>
+                    <input type="radio" id="farmer" name="userGroup" value="Farmer" />
+                    <label for="farmer">Farmer</label>
+                </div>
+            </fieldset>
+            <form className="form-container">
+                <div className="form-sub">
+                    <label for="contact">Contact Number:</label>
+                    <input type='text' name="contact" id="contact" onChange={(e) => {setContact(e.target.value)}}/>
+                </div>
+                <div className="form-sub">
+                    <label for="password">Password:</label>
+                    <input type='password' name="password" id="password" onChange={(e) => {setPassword(e.target.value)}}/>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Login
