b50667958b5cc7a9bb34e835a22079e6e0e93ed0
rahul-on-git <2020.rahul.motwani@ves.ac.in>
2023-03-02 18:46:29 +0530

    added partial auth to farmer signup

--- Controllers/farmer.js
+++ Controllers/farmer.js
@@ -14,6 +14,13 @@ exports.signup = (req, res, next) => {
     let farmerLocation = req.body.farmerLocation;
     let farmerPass = req.body.farmerPass;
 
+    if(!farmerName || !farmerContact || !farmerLocation || !farmerPass){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+
+    const userAvailable = Farmer.findOne({farmerContact})
+
     const farmer = new Farmer({ farmerName: farmerName, farmerContact: farmerContact, farmerLocation: farmerLocation, farmerPass: farmerPass });
 
     farmer
